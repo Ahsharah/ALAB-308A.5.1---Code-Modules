cc8a05c97a4e044ae6fd5a44c309974a70b4cffd
Alexandria <walkerciara24@gmail.com>
2024-10-21 17:39:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ const data = [
 { id: "63", name: "Blaine", ocupation: "Quiz Master", age:"58" },
 { id: "7", name: "Bilbo", occupation: "None", age: "111" },
 ];
+
+// Test API functions
+console.log("\nTesting API functions:");
+console.log("Sorted by age:", sortByAge([...data]));
+console.log("Filtered age <= 50:", filterAgeAbove50(data));
+console.log("Transformed data:", transformData(data));
+
+const ageStats = caluculateAgeStats(data);
+console.log("Age statistics:");
+console.log(`Sum of ages: ${ageStats.sum}`);
+console.log(`Average age: ${ageStats.average.toFixed(2)}`);
