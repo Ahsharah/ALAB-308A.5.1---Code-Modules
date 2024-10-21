f021a647f933de3263f48dc1677b98a0edcd1f0f
Alexandria <walkerciara24@gmail.com>
2024-10-21 17:13:08 -0400

    Update app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+// Sample data for API functions
+const data = [
+{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+{ id: "48", name: "Barry", occupation: "Runner", age: "25" },
+{ id: "57",  name: "Bob", occupation: "Fry Cook", age: "19" },
+{ id: "63", name: "Blaine", ocupation: "Quiz Master", age:"58" },
+{ id: "7", name: "Bilbo", occupation: "None", age: "111" },
+];
